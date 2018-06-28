fa45cd0bfc4aa4ab93265bd00aaf84e894b5aa6b
serafinerubio <40475931+serafinerubio@users.noreply.github.com>
2018-06-28 13:18:57 -0700

    Add files via upload

---
+++ labs/formlab.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    function processForm(e) {
+
+
+      e.preventDefault();
+
+      var data = document.getElementById("input").value;
+   
+
+
+    }
+var form = document.getElementById("myForm");
+form.addEventListener("submit", processForm);
+}
